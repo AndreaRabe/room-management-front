1d3c27505a000563d18d4dff681b05c221cfc1a9
Andrea Rabe <andrearaberabe@gmail.com>
2023-11-25 10:16:49 +0300

    SingIn Admin and checkBox

--- src/pages/singIn/singIn.jsx
+++ src/pages/singIn/singIn.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/header/header"
 import SinInImg from '../../assets/images/signIn.png';
 import './singIn.css'
+import { useState } from "react";
 
 
 const departement = ["Informatique", "Ressources Humaines", "Patrimoine"]
@@ -23,11 +24,20 @@ export function SingIn(){
 }
 
 function SingInForm(){
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    function handleTabClick(value){
+        setIsAdmin(!isAdmin)
+    }
     return(
         <div className="form-singin">
             <div className="form-singin-div-1">
                 <span className="form-singin-span">Créer un compte</span>
-                    <SingInFormClient />
+                <div className="form-singin-div-1-checkbox">
+                    <span style={{ marginRight: '100px' }} className={!isAdmin ? 'active': ''} onClick={handleTabClick}>Client</span>
+                    <span style={{ marginLeft: '100px' }} className={isAdmin ? 'active': ''} onClick={handleTabClick}>Administrateur</span>
+                </div>
+                {!isAdmin ? <SingInFormClient /> : <SingInFormAdmin/>}
                 <div className="form-singin-div-3">
                     Vous avez déjà un compte ?<b className="form-singin-div-3-b">Se connecter</b>
                 </div>
@@ -42,23 +52,35 @@ function SingInFormClient(){
                 <form className="formClient-singin-div-2">
 
                     <div className="formClient-singin-div-4">
+
+                        <div className="formClient-singin-input-group">
                         <input type="text" placeholder="Email" className="formClient-singin-input" />
                         <input type="text" placeholder="Nom" className="formClient-singin-input" />
-                        <input type="text" placeholder="Prenom" className="formClient-singin-input" />
+                        </div>
+
+
+                        <div className="formClient-singin-input-group">
+                        <input type="text" placeholder="Prénom" className="formClient-singin-input" />
                         {/* <div className="content-tel"> */}
                             {/* <span className="formClient-singin-input-tel-prefix" >+261</span> */}
-                            <input type="text" placeholder="Tel +261" className="formClient-singin-input" />
                         {/* </div> */}
+                            <input type="text" placeholder="Tel +261" className="formClient-singin-input" />
+                        </div>
 
+                        <div className="formClient-singin-input-group">
                         <input type="password" placeholder="Mot de passe" className="formClient-singin-input" />
                         <input type="password" placeholder="Confirmer mot de passe" className="formClient-singin-input" />
+                        </div>
 
+
+                        <div className="formClient-singin-input-group">
                         <input type="text" placeholder="Entreprise" className="formClient-singin-input" />
 
                         <select id="deparement" name="departement" className="formClient-singin-dropdown">
                             <option value="" disabled selected>Votre departement</option>
                             {departement.map((item, key)=> <option value={item} key={{key}}>{item}</option> )}
                         </select>
+                        </div>                        
 
                         <button className="formClient-singin-btn">Creer compte</button>
                     </div>
@@ -70,20 +92,44 @@ function SingInFormClient(){
 function SingInFormAdmin(){
     return(
         <div>
-            <form className="form-singin-div-2">
-                <span className="form-singin-span">Créer un compte</span>
+                <form className="formAdmin-singin-div-2">
 
-                <div className="form-singin-div-4">
-                    <input type="text" placeholder="Email" className="form-singin-input" />
-                    <input type="password" placeholder="Mot de passe" className="form-singin-input" />
-                    <div className="form-singin-input-span" >Mot de passe oublié ?</div>
-                    <button className="form-singin-btn">Creer compte</button>
+                    <div className="formAdmin-singin-div-4">
 
-                </div>
-            </form>
-            <div className="form-singin-div-3">
-                Vous avez déjà un compte ?<b className="form-singin-div-3-b">Se connecter</b>
-            </div>
+                        <div className="formAdmin-singin-input-group">
+                            <input type="text" placeholder="Email" className="formAdmin-singin-input" />
+                            <input type="text" placeholder="Nom" className="formAdmin-singin-input" />
+                        </div>
+
+
+                        <div className="formAdmin-singin-input-group">
+                            <input type="text" placeholder="Prénom" className="formAdmin-singin-input" />
+                            <input type="text" placeholder="IM" className="formAdmin-singin-input" />
+                        </div>
+
+                        <div className="formAdmin-singin-input-group">
+                            <input type="text" placeholder="Fonction" className="formAdmin-singin-input" />
+                            {/* <div className="content-tel"> */}
+                                {/* <span className="formAdmin-singin-input-tel-prefix" >+261</span> */}
+                            {/* </div> */}
+                            <input type="text" placeholder="Tel +261" className="formAdmin-singin-input" />
+                        </div>
+                        <div className="formAdmin-singin-input-group">
+                            <input type="password" placeholder="Mot de passe" className="formAdmin-singin-input" />
+                            <input type="password" placeholder="Confirmer mot de passe" className="formAdmin-singin-input" />
+                        </div>
+
+
+                        <div className="formAdmin-singin-input-group">
+                            <select id="deparement" name="departement" className="formAdmin-singin-dropdown">
+                                <option value="" disabled selected>Votre departement</option>
+                                {departement.map((item, key)=> <option value={item} key={{key}}>{item}</option> )}
+                            </select>
+                        </div>                        
+
+                        <button className="formClient-singin-btn">Creer compte</button>
+                    </div>
+                </form>
         </div>
     )
 }
