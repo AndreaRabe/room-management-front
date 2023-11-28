20b164f60e56bdf52a59b0e21f575ba3d34276cc
Andrea Rabe <andrearaberabe@gmail.com>
2023-11-28 22:59:11 +0300

    login integration

--- src/pages/singUp/singUp.jsx
+++ src/pages/singUp/singUp.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/header/header"
 import SinUpImg from '../../assets/images/singUp.png';
 import './singUp.css'
+import { SingUpForm } from "../../utils/test";
 export function SingUp() {
     return(
         <div className="pg-singup">
@@ -18,24 +19,24 @@ export function SingUp() {
     )
 }
 
-function SingUpForm(){
-    return(
-        <div className="form-singup">
-            <div className="form-singup-div-1">
-                <form className="form-singup-div-2">
-                    <span className="form-singup-span">Se connecter</span>
-                    <div className="form-singup-div-4">
-                        <input type="text" placeholder="Email" className="form-singup-input" />
-                        <input type="password" placeholder="Mot de passe" className="form-singup-input" />
-                        <div className="form-singup-input-span" >Mot de passe oublié ?</div>
-                        <button className="form-singup-btn">Se connecter</button>
+// function SingUpForm(){
+//     return(
+//         <div className="form-singup">
+//             <div className="form-singup-div-1">
+//                 <form className="form-singup-div-2">
+//                     <span className="form-singup-span">Se connecter</span>
+//                     <div className="form-singup-div-4">
+//                         <input type="text" placeholder="Email" className="form-singup-input" />
+//                         <input type="password" placeholder="Mot de passe" className="form-singup-input" />
+//                         <div className="form-singup-input-span" >Mot de passe oublié ?</div>
+//                         <button className="form-singup-btn">Se connecter</button>
 
-                    </div>
-                </form>
-                <div className="form-singup-div-3">
-                    Vous n'avez pas de compte ?<b className="form-singup-div-3-b">Créer un compte</b>
-                </div>
-            </div>
-        </div>
-    )
-}
+//                     </div>
+//                 </form>
+//                 <div className="form-singup-div-3">
+//                     Vous n'avez pas de compte ?<b className="form-singup-div-3-b">Créer un compte</b>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
--- src/utils/test.jsx
+++ src/utils/test.jsx
@@ -280,3 +280,79 @@ import React, { useState } from 'react';
 //       </div>
 //   )
 // }
+
+// signUp form 
+
+export function SingUpForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          username: email,
+          password: password,
+          grant_type: '',
+          scope: '',
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const accessToken = data.access_token;
+
+      // Store the token where needed (localStorage, global state, etc.)
+      localStorage.setItem('access_token', accessToken);
+
+      // Redirect the user to the desired page or state after successful login
+      // window.location.href = '/dashboard';
+      console.log('Successfully logged in! Token:', accessToken);
+    } else {
+      const errorData = await response.json();
+      console.error('Login failed:', errorData.detail);
+    }
+  } catch (error) {
+    console.error('Error during the request:', error);
+  }
+};
+
+  return (
+    <div className="form-singup">
+      <div className="form-singup-div-1">
+        <form className="form-singup-div-2">
+          <span className="form-singup-span">Se connecter</span>
+          <div className="form-singup-div-4">
+            <input
+              type="text"
+              placeholder="Email"
+              className="form-singup-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              className="form-singup-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="form-singup-input-span">Mot de passe oublié ?</div>
+            <button className="form-singup-btn" onClick={handleLogin}>
+              Se connecter
+            </button>
+          </div>
+        </form>
+        <div className="form-singup-div-3">
+          Vous n'avez pas de compte ?<b className="form-singup-div-3-b">Créer un compte</b>
+        </div>
+      </div>
+    </div>
+  );
+}
+
